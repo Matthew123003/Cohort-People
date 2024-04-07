c39e2d7aec283aeee8b535b34a94df01b39f8853
Matthew123003 <160056522+Matthew123003@users.noreply.github.com>
2024-04-07 00:03:07 +0000

    Finished

--- src/main/java/zipcode/rocks/Main.java
+++ src/main/java/zipcode/rocks/Main.java
@@ -17,7 +17,7 @@ public class Main {
         // Load data file into an ArrayList<Person> of Person objects.
         Main programObject = new Main();
 
-        List listOfStudents = programObject.loadCSVFile(student_source);
+        List listOfStudents = programObject.handWrittenLoadOfData();
 
         int numberOfPersons = listOfStudents.size();
         for (int i = 0; i< numberOfPersons; i++){
@@ -48,4 +48,52 @@ public class Main {
         return newPerson;
     }
 
+
+private List handWrittenLoadOfData() {
+    ArrayList<Person> students = new ArrayList<>();
+    Person newPerson = new Person("Abukar", "Abukar");
+    newPerson.setgithubHandle("abukarabukar");
+    newPerson.setfavColor("yellow");
+    newPerson.setbirthMonth("April");
+    newPerson.sethasPets(true);
+    students.add(newPerson);
+
+    Person newPerson1 = new Person("Chaz", "Adams");
+    newPerson1.setgithubHandle("chaz_adams");
+    newPerson1.setfavColor("blue");
+    newPerson1.setbirthMonth("February");
+    newPerson1.sethasPets(true);
+    students.add(newPerson1);
+
+    Person newPerson2 = new Person("Asan", "Ahmadli");
+    newPerson2.setgithubHandle("asanahamdli");
+    newPerson2.setfavColor("dark blue");
+    newPerson2.setbirthMonth("December");
+    newPerson2.sethasPets(false);
+    students.add(newPerson2);
+
+    Person newPerson3 = new Person("Ali", "Bangash");
+    newPerson3.setgithubHandle("ali-b14");
+    newPerson3.setfavColor("green");
+    newPerson3.setbirthMonth("March");
+    newPerson3.sethasPets(false);
+    students.add(newPerson3);
+
+    Person newPerson4 = new Person("Jenn", "Azer");
+    newPerson4.setgithubHandle("cupofjavajenny");
+    newPerson4.setfavColor("purple");
+    newPerson4.setbirthMonth("November");
+    newPerson4.sethasPets(true);
+    students.add(newPerson4);
+
+    Person newPerson5 = new Person("Jake", "Brennan");
+    newPerson5.setgithubHandle("jake-brennan");
+    newPerson5.setfavColor("purple");
+    newPerson5.setbirthMonth("April");
+    newPerson5.sethasPets(true);
+    students.add(newPerson5);
+    
+    // and so on for EACH person in the cohort.
+    return students;
+  }
 }
--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -3,10 +3,15 @@ package zipcode.rocks;
 public class Person {
     private String firstname;
     private String lastname;
+    private String favColor;
+    private String birthMonth;
+    private String githubHandle;
+    private boolean hasPets;
 
     public Person(String fname, String lname) {
         this.firstname = fname;
         this.lastname = lname;
+
     }
 
 
@@ -26,7 +31,31 @@ public class Person {
         this.lastname = lastname;
     }
 
-    public String toString() {
-        return this.lastname + ", " + this.firstname;
+    public String getfavColor(){
+      return favColor;
     }
+    public void setfavColor(String favColor){
+      this.favColor = favColor;
+    }
+    public String getbirthMonth(){
+      return birthMonth;
+    }
+    public void setbirthMonth(String birthMonth){
+      this.birthMonth = birthMonth;
+    }
+    public String getgithubHandle(){
+      return githubHandle;
+    }
+    public void setgithubHandle(String githubHandle){
+      this.githubHandle = githubHandle;
+    }
+    public boolean gethasPets(){
+      return hasPets;
+    }
+    public void sethasPets(boolean hasPets){
+      this.hasPets = hasPets;
+    }
+    public String toString() {
+        return this.lastname + ", " + this.firstname + ", " + this.birthMonth + ", " + this.favColor + ", " + this.githubHandle + ", " + this.hasPets;
+  }
 }
